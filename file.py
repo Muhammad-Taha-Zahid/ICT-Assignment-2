b5bb114e2b48937de73242892967978e25ea1bec
Muhammad Muthar <mnoonari.bese25seecs@seecs.edu.pk>
2025-11-28 15:44:34 +0500

    Added Guessing game

--- file.py
+++ file.py
@@ -1,3 +1,5 @@
+import random
+
 def Smart_Calculator():
     print("Smart Calculator")
     
@@ -38,4 +40,41 @@ def Smart_Calculator():
     print(f"The calculation is: {n1} {operation} {n2}")
     print(f"The result is: {result}")
 
-Smart_Calculator()
+
+def guessing_game():
+    print("\t***** WELCOME to \"GUESS THE NUMBER!\" *****")
+    print("The number is between 1 and 50!\n")
+    x = random.randint(1, 50)
+    attempts = 1
+    while True:
+        try:
+            guess = int(input("Enter a number to start playing:\n"))
+        except ValueError:
+            print("Invalid input! Please enter a number.")
+            continue
+        if 1 <= guess <= 50:
+            if guess < x:
+                print("Too Low! Try Higher!\n")
+                attempts += 1
+            elif guess > x:
+                print("Too High! Try Lower!\n")
+                attempts += 1
+            else:
+                print(f"Congratulations! You guessed the number {x}!")
+                print(f"It took you {attempts} attempts.\n")
+                break
+        else:
+            print("Invalid Input. Enter a number between 1 and 50.\n")
+
+def main():
+    print("1. Smart Calculator")
+    print("2. Guessing Game")
+    choice = input("Choose option: ")
+    if choice == "1":
+        Smart_Calculator()
+    elif choice == "2":
+        guessing_game()
+    else:
+        print("Invalid choice")
+
+main()
